4651e3712bf9e41a8e21d1dbb547739fe60de9bd
Админ <admin@MacBook-Pro-Admin.local>
2023-07-18 15:44:45 +0300

    first commit tailwind quick app with react and typescript

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '',
+      },
+      fontFamily: {
+        custom: ['you font name'],
+      },
+    },
+  },
+  plugins: [],
+};
